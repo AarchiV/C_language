903c9a06d4ce659f5c7237e79d1a6e068ed139af
AarchiV <vachhaniaarchi@gmail.com>
2023-11-03 20:01:19 +0530

    03|11|23

---
+++ CH-8/8-3/QUESTION-1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	 int n=1;
+	 clrscr();
+	 do
+	 {
+	   printf("%d\n",n++);
+	 }while(n<=10);
+	 getch();
+}
---
+++ CH-8/8-3/QUESTION-3.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+       int n,a=1;
+       clrscr();
+       printf("Enter any value : ");
+       scanf("%d",&n);
+       do
+       {
+	 if(a%2==0)
+	 {
+	   printf("%d\n",a);
+	 }
+	 a++;
+       }while(a<=n);
+       getch();
+}
