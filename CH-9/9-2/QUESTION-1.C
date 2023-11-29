1dcaa6e62cd3200ef8a5a4c8f0e0467ad511f2d5
AarchiV <vachhaniaarchi@gmail.com>
2023-11-29 19:33:13 +0530

    29|11|23

--- CH-9/9-2/QUESTION-1.C
+++ CH-9/9-2/QUESTION-1.C
@@ -3,19 +3,19 @@
 
 main()
 {
-	      int i,j,s;
-	      clrscr();
-	      for(i=5;i>=1;i--)
-	      {
-		for(s=1;s<i;s++)
+		int i,j,s;
+		clrscr();
+		for(i=1;i<=5;i++)
 		{
-		  printf(" ");
+		  for(s=5;s>i;s--)
+		  {
+		    printf(" ");
+		  }
+		  for(j=i;j>=1;j--)
+		  {
+		    printf("%d",j);
+		  }
+		  printf("\n");
 		}
-		for(j=i;j<=5;j++)
-		{
-		  printf("%d",i);
-		}
-		printf("\n");
-	      }
-	      getch();
+		getch();
 }
